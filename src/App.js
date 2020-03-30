68c14a303b75e3d0e9e3223f8e32d7d07c0091fe
Corey Knoettgen <cknoettg@yahoo.com>
2020-03-29 21:11:05 -0400

    Reconceived structure. Added overarching card.js. Added imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import ".components/Card";
 
 function App() {
   return (
     <div className="App">
-      <h1>NASA Photo of the Day</h1>
-      
+      <h1 class="App-logo">NASA Photo of the Day</h1>
+
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+//will contain all of the smaller components
+import React, { useState, useEffect } from "react";
+import ".components/Title";
+import ".components/Date";
+import ".components/Image";
+import ".components/Desc";
+import ".components/Footer";
